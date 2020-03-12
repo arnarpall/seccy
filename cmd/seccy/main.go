7779f6b7c68b0ca1890522f9db76b74fc1b3989e
Arnar Páll <arnar@netapp.com>
2020-03-12 13:57:31 +0000

    Fixing logging

--- cmd/seccy/main.go
+++ cmd/seccy/main.go
@@ -1,22 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/arnarpall/seccy/cmd/seccy/cmd"
 	"github.com/arnarpall/seccy/internal/log"
 	"github.com/arnarpall/seccy/pkg/client"
 	"github.com/spf13/cobra"
+	"go.uber.org/zap"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "seccy",
-	Short: "Client is a crazy secrets keeper",
+	Short: "Secrets keeper",
 }
 
 func main() {
-	logger := log.New()
+	lc, _:= zap.NewDevelopment()
+	logger := &log.Logger{
+		SugaredLogger: lc.Sugar(),
+	}
+
 	c, err := client.New(":4040", logger)
 	if err != nil {
 		logger.Fatalf("Unable to connect to seccy server, make sure that the server has been started", "error", err)
@@ -26,7 +28,6 @@ func main() {
 	rootCmd.AddCommand(cmd.CreateSetCommand(c))
 
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		logger.Fatal(err)
 	}
 }
